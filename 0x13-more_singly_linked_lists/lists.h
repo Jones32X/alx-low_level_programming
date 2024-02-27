db03510dca14a72e0c63de331369a6f3ddd9fd82
Jones32X <jonespedro.Nezingu@gmail.com.com>
2024-02-27 04:04:21 -0800

    fixed lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,12 +14,11 @@
  * @next: directs to the following node
  *
  * Description: singly linked list node structure
- * for alx project
  */
 typedef struct listint_s
 {
- int n;
- struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
